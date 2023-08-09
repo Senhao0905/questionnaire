3716e703ccebea9de1c04da2deaab4371673c968
Senhao0905 <senhao0905@outlook.com>
2023-08-09 21:55:59 +0800

    del test  try put on Zeabur

--- src/test/java/com/example/questionnaire/QuestionniareTest.java
+++ src/test/java/com/example/questionnaire/QuestionniareTest.java
@@ -17,45 +17,14 @@ import com.example.questionnaire.service.ifs.QuestionnaireService;
 class QuestionniareTest {
 
 	
-	@Autowired
-	private QuestionnaireService questionnaireService;
-
-	@Autowired
-	private QuestionnaireDao questionnaireDao;
+	
 	
 	
 	@Test
 	public void addQuestionniareTest() {
-		String name = "測試問卷";
-		String describe = "測試問卷";
-		LocalDate startDate = LocalDate.of(2023, 6, 19);
-		LocalDate endDate = LocalDate.of(2023, 6, 30);
-		Questionnaire questionnaire = new Questionnaire(name,describe, startDate, endDate);
-		questionnaireService.addQuestionnaire(questionnaire);
+		
 	}
 	
-	@Test
-	public void delQuestioniareTest() {
-		int id = 3;
-		questionnaireService.delQuestionnaire(id);
-	}
-	
-	@Test
-	public void searchByNameTest() {
-		String name = "";
-		LocalDate start = LocalDate.of(2023, 6, 30);
-		LocalDate end = LocalDate.of(2023, 7, 02);
-		questionnaireService.searchByDateOrName(name,start,end,0);
-	}
-	
-	
-	@Test
-	public void searchByDateTest() {
-		LocalDate startDate = LocalDate.of(2023, 6, 19);
-		LocalDate endDate = LocalDate.of(2023, 6, 23);
-//		List<Questionnaire> res = questionnaireDao.searchByDate(startDate, endDate);
-		System.out.println();
-	}
 	
 
 }
